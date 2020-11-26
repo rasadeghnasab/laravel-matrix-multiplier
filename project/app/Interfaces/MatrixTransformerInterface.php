d5554697f67428a4189252e34cf15db9eb7345d5
Ramin <rasadeghnasab@gmail.com>
2020-11-26 17:15:48 +0330

    - Add MatrixMultiplier - Add MatrixOperator - Add MatrixOperatorInterface - Add MartixCharTransformer

---
+++ project/app/Interfaces/MatrixTransformerInterface.php
@@ -0,0 +1,9 @@
+<?php
+
+
+namespace App\Interfaces;
+
+interface MatrixTransformerInterface
+{
+    public static function transformer();
+}
---
+++ project/app/Transformers/MatrixCharTransformer.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace App\Classes\Transformers;
+
+class MatrixCharTransformer implements TransformerInterface
+{
+
+}
